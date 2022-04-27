938be1d1bc3af91046e879a354fa30bd5807406b
Ali Sheykhi <alishi.dev@gmail.com>
2022-04-27 18:06:46 +0430

    Implement Weather ViewModel with Hilt And Coroutine

---
+++ app/src/main/java/ir/ali/weatherforecast/view/viewModel/WeatherViewModel.kt
@@ -0,0 +1,39 @@
+package ir.ali.weatherforecast.view.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import ir.ali.weatherforecast.model.Weather
+import ir.ali.weatherforecast.service.repository.WeatherRepository
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WeatherViewModel @Inject constructor(private val weatherRepository: WeatherRepository) : ViewModel() {
+
+    private val _weather: MutableLiveData<Weather> = MutableLiveData()
+    val weather: LiveData<Weather> get() = _weather
+
+    private val _loading: MutableLiveData<Boolean> = MutableLiveData(false)
+    val loading: LiveData<Boolean> get() = _loading
+
+    private val _error: MutableLiveData<Exception> = MutableLiveData()
+    val error: LiveData<Exception> get() = _error
+
+    fun loadWeatherForecastData() {
+        viewModelScope.launch {
+            _loading.value = true
+            try {
+                _weather.value = weatherRepository.getWeather()
+            } catch (e: Exception) {
+                _error.value = e
+                e.printStackTrace()
+            } finally {
+                _loading.value = false
+            }
+        }
+    }
+
+}
