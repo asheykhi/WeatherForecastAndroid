0aace49c75527c06905ce43c599ccdf4699bf834
Ali Sheykhi <alishi.dev@gmail.com>
2022-04-27 17:39:12 +0430

    Implement Network Module Hilt Base

---
+++ app/src/main/java/ir/ali/weatherforecast/di/NetworkModule.kt
@@ -0,0 +1,39 @@
+package ir.ali.weatherforecast.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import ir.ali.weatherforecast.service.api.WeatherService
+import ir.ali.weatherforecast.utils.Cosntants.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideGsonBuilder(): Gson =
+        GsonBuilder()
+            .excludeFieldsWithoutExposeAnnotation()
+            .create()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(gson: Gson): Retrofit.Builder =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+
+    @Singleton
+    @Provides
+    fun provideWeatherService(retrofit: Retrofit.Builder): WeatherService =
+        retrofit
+            .build()
+            .create(WeatherService::class.java)
+}
