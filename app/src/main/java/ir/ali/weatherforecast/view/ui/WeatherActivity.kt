dec9c9f23585c4f9c2175936d34c41d6a5887847
Ali Sheykhi <alishi.dev@gmail.com>
2022-04-27 21:45:15 +0430

    Use SerachView And Update Ui

--- app/src/main/java/ir/ali/weatherforecast/view/ui/WeatherActivity.kt
+++ app/src/main/java/ir/ali/weatherforecast/view/ui/WeatherActivity.kt
@@ -1,11 +1,14 @@
 package ir.ali.weatherforecast.view.ui
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.View.*
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import dagger.hilt.android.AndroidEntryPoint
+import ir.ali.weatherforecast.R
 import ir.ali.weatherforecast.databinding.ActivityWeatherBinding
 import ir.ali.weatherforecast.model.Weather
 import ir.ali.weatherforecast.view.viewModel.WeatherViewModel
@@ -32,16 +35,31 @@ class WeatherActivity : AppCompatActivity() {
         binding = ActivityWeatherBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupObservers(viewModel)
-
-        binding.fetchBtn.setOnClickListener {
-            viewModel.loadWeatherForecastData(binding.tfCityName.text.toString())
-        }
     }
-
 
     private fun setupObservers(viewModel: WeatherViewModel) {
         viewModel.error.observe(this, errorObserver)
         viewModel.weather.observe(this, dataObserver)
         viewModel.loading.observe(this, loadingObserver)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        val menuItem = menu?.findItem(R.id.search_bar)
+        val searchView = menuItem?.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                viewModel.loadWeatherForecastData(query.toString())
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+
+            }
+
+        })
+        return super.onCreateOptionsMenu(menu)
     }
 }
